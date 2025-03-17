05833808f0da527e3ad92a816138f1be54320886
Zain Hasan <zain.hasan@kravemart.com>
2025-03-17 16:29:33 +0500

    cleaning

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -29,10 +29,10 @@ const Home = () => {
 
       {/* Other Components */}
       <div className="p-8">
-        <h2 className="text-2xl font-bold mb-4">More Content</h2>
+        {/* <h2 className="text-2xl font-bold mb-4">More Content</h2>
         <p className="text-gray-700">
           This content is outside the background image and will scroll normally.
-        </p>
+        </p> */}
 
         <Feature />
         <Advantages />
